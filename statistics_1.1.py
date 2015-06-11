f2f5c99e9dc5ab4991445f647cd24775fb619cb5
gregorleonhard@web.de <gregorleonhard@web.de>
2015-06-11 15:55:35 +0200

    Standardabweichung berechnet

--- statistics_1.1.py
+++ statistics_1.1.py
@@ -1,4 +1,5 @@
 import sys
+import math
 def m(values):
 	x = 0
 	y = 0
@@ -9,13 +10,19 @@ def m(values):
 		z = z + float(v[2])
 	return [x/len(values), y / len(values), z / len(values)]
 
-
+def std(values,average):
+    x,y,z = 0,0,0
+    for v in values:
+        x += (float(v[0])-average[0])**2
+        y += (float(v[1])-average[1])**2
+        z += (float(v[2])-average[2])**2
+    return[math.sqrt(x/len(values)),math.sqrt(y/len(values)),math.sqrt(z/len(values))]
 
 
 window_size = int(sys.argv[2])
 templist = []
 mittelwerte = []
-
+standardabweichung = []
 import csv 
 with open(sys.argv[1]) as csvfile:
        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -25,10 +32,16 @@ with open(sys.argv[1]) as csvfile:
           vector = [x,y,z]
           templist.append(vector)
           if t % window_size == 0 and t!=0:
-             mittelwerte.append(m(templist))
+             average = m(templist)
+             mittelwerte.append(average)
+             standardabweichung.append(std(templist,average))
              templist = []
 
 for x in mittelwerte:
 	print (x)
+print("\n")
+
+for x in standardabweichung:
+        print(x)
 
 
