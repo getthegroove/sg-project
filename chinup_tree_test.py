fd7f0e71085c689cc339d95b875d52835a908025
gregorleonhard@web.de <gregorleonhard@web.de>
2015-06-30 11:09:58 +0200

    some stuff

---
+++ chinup_tree_test.py
@@ -0,0 +1,27 @@
+import pickle
+from sklearn import tree
+import sys
+import csv
+
+files = []
+result = []
+filename = sys.argv[1]
+clf = pickle.load(open("save_"+filename+".p","rb"))
+
+
+with open(sys.argv[2],"r") as csvfile:
+     reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+     for row in reader:
+         files = files + [row[2:8]]
+
+result = clf.predict(files)
+
+for i in result:
+    if (i==0):
+       print("static"+"\n")
+    elif (i==1):
+       print("right-left"+"\n")
+    elif (i==2):
+       print("chinup"+"\n")
+    elif (i==3):
+       print("up_down"+"\n")
--- plot.py
+++ plot.py
@@ -20,10 +20,11 @@ with open(sys.argv[1]) as csvfile:
          norm.append(math.sqrt(float(row[1])**2+float(row[1])**2+float(row[3])**2))
          
 
-plt.plot(temps,X)
-plt.plot(temps,Y)
-plt.plot(temps,Z)
-plt.plot(temps,norm)
+plt.plot(temps,X,label="X")
+plt.plot(temps,Y,label="Y")
+plt.plot(temps,Z,label="Z")
+plt.plot(temps,norm,label="norm")
 plt.xlabel("time[ms]")
 plt.ylabel("accelration[m/s^2]")
+plt.legend(bbox_to_anchor=(1.05, 1), loc=1, borderaxespad=0.)
 plt.show()
