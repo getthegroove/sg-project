b4d4f0de0626e7cc2576c036d463acb991e95cc0
gregorleonhard@web.de <gregorleonhard@web.de>
2015-07-21 10:05:17 +0200

    added all files

--- plot.py
+++ plot.py
@@ -5,26 +5,34 @@ import matplotlib.pyplot as plt
 import math
 
 temps = []
-X = []
-Y = []
-Z = []
-norm = []
+X_av = []
+Y_av = []
+Z_av = []
+X_st = []
+Y_st = []
+Z_st = []
+
 
 with open(sys.argv[1]) as csvfile:
        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
        for row in reader:
-         temps.append(row[0])
-         X.append(row[1])
-         Y.append(row[2])
-         Z.append(row[3])
-         norm.append(math.sqrt(float(row[1])**2+float(row[1])**2+float(row[3])**2))
+         temps.append(row[1])
+         X_av.append(row[2])
+         Y_av.append(row[3])
+         Z_av.append(row[4])
+         X_st.append(row[5])
+         Y_st.append(row[6])
+         Z_st.append(row[7])
+         
          
 
-plt.plot(temps,X,label="X")
-plt.plot(temps,Y,label="Y")
-plt.plot(temps,Z,label="Z")
-plt.plot(temps,norm,label="norm")
+plt.plot(temps,X_av,label="X_av")
+plt.plot(temps,Y_av,label="Y_av")
+plt.plot(temps,Z_av,label="Z_av")
+plt.plot(temps,X_st,label="X_st")
+plt.plot(temps,Y_st,label="Y_st")
+plt.plot(temps,Z_st,label="Z_st")
 plt.xlabel("time[ms]")
 plt.ylabel("accelration[m/s^2]")
-plt.legend(bbox_to_anchor=(1.05, 1), loc=1, borderaxespad=0.)
+plt.legend(bbox_to_anchor=(1.00, 1), loc=1, borderaxespad=0.)
 plt.show()
